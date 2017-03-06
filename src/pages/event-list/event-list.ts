db9da9e800253cdb5a21d94c11a532e0a12f8a6d
Juarez Filho <juarezpaf@gmail.com>
2017-03-06 01:50:43 -0300

    feat: list events using ngFor with Async pipe

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Event } from "api/models/app-models";
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import 'rxjs/add/operator/map'
+
 import { EventCreatePage } from '../event-create/event-create';
 
 @Component({
@@ -7,11 +11,19 @@ import { EventCreatePage } from '../event-create/event-create';
   templateUrl: 'event-list.html'
 })
 export class EventListPage {
+  events: FirebaseListObservable<Event>;
+  eventsCount: number;
 
   constructor(
+    public af: AngularFire,
     public navCtrl: NavController,
     public navParams: NavParams
-  ) {}
+  ) {
+    this.events = this.af.database.list('/events');
+    this.af.database.list('/events').map(list=>list.length).subscribe(length => {
+      this.eventsCount = length;
+    });
+  }
 
   pushToEventCreate() {
     this.navCtrl.push(EventCreatePage);
